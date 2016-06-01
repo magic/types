d201369e7ba3c986c9bfcd893354d1bdcfed1b45
Jascha Ehrenreich <jascha@jaeh.at>
2016-06-01 21:26:40 +0200

    add dist and sourcemap

--- index.js
+++ index.js
@@ -6,65 +6,43 @@ Object.defineProperty(exports, "__esModule", {
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
-var objProto = Object.prototype;
-var toStr = objProto.toString;
-
-var cleanType = exports.cleanType = function cleanType(t) {
-  return t === 'array' && '[object Array]' || t.toLowerCase() === 'nodelist' && '[object NodeList]' || t === 'object' && '[object Object]' || t;
-};
-
-var cleanTypes = exports.cleanTypes = function cleanTypes() {
-  for (var _len = arguments.length, addTypes = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-    addTypes[_key - 1] = arguments[_key];
-  }
-
-  var types = arguments.length <= 0 || arguments[0] === undefined ? [] : arguments[0];
-
-  var allTypes = addTypes.concat(types);
-  var newTypes = allTypes.map(function (t) {
-    return cleanType(t);
-  });
-
-  return newTypes.length === 1 ? newTypes[0] : newTypes;
-};
-
 var test = exports.test = function test(ele) {
-  for (var _len2 = arguments.length, addTypes = Array(_len2 > 2 ? _len2 - 2 : 0), _key2 = 2; _key2 < _len2; _key2++) {
-    addTypes[_key2 - 2] = arguments[_key2];
+  for (var _len = arguments.length, addTypes = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    addTypes[_key - 2] = arguments[_key];
   }
 
   var types = arguments.length <= 1 || arguments[1] === undefined ? [] : arguments[1];
 
   if (isString(types) && isEmpty(addTypes)) {
-    var type = cleanType(types);
-    return toStr.call(ele) === type || (typeof ele === 'undefined' ? 'undefined' : _typeof(ele)) === type;
+    var type = types;
+    return Object.prototype.toString(ele) === type || (typeof ele === 'undefined' ? 'undefined' : _typeof(ele)) === type;
   }
 
-  var allTypes = addTypes.concat(types);
-  var tested = allTypes.some(function (t) {
+  var tested = addTypes.concat(types).some(function (t) {
     return test(ele, t);
   });
+
   return tested;
 };
 
 var is = exports.is = function is(ele) {
-  for (var _len3 = arguments.length, types = Array(_len3 > 1 ? _len3 - 1 : 0), _key3 = 1; _key3 < _len3; _key3++) {
-    types[_key3 - 1] = arguments[_key3];
+  for (var _len2 = arguments.length, types = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+    types[_key2 - 1] = arguments[_key2];
   }
 
   return test(ele, types);
 };
 
 var not = exports.not = function not(ele) {
-  for (var _len4 = arguments.length, types = Array(_len4 > 1 ? _len4 - 1 : 0), _key4 = 1; _key4 < _len4; _key4++) {
-    types[_key4 - 1] = arguments[_key4];
+  for (var _len3 = arguments.length, types = Array(_len3 > 1 ? _len3 - 1 : 0), _key3 = 1; _key3 < _len3; _key3++) {
+    types[_key3 - 1] = arguments[_key3];
   }
 
   return !test(ele, types);
 };
 
 var isArray = exports.isArray = function isArray(ele) {
-  return toStr.call(ele) === '[object Array]';
+  return isFunction(ele.forEach);
 };
 
 var isBoolean = exports.isBoolean = function isBoolean(ele) {
@@ -80,11 +58,19 @@ var isFunction = exports.isFunction = function isFunction(ele) {
 };
 
 var isNumber = exports.isNumber = function isNumber(ele) {
-  return parseFloat(ele, 10) === parseFloat(ele, 10);
+  return ele === +ele;
+};
+
+var isInteger = exports.isInteger = function isInteger(ele) {
+  return ele === +ele && ele === (ele | 0);
+};
+
+var isFloat = exports.isFloat = function isFloat(n) {
+  return n === +n && n !== (n | 0);
 };
 
 var toInt = exports.toInt = function toInt(ele) {
-  return isNumber(ele) && parseInt(ele, 10);
+  return isNumber(ele) && ele | 0;
 };
 
 var toFloat = exports.toFloat = function toFloat(ele) {
@@ -145,10 +131,6 @@ var isError = exports.isError = function isError(ele) {
   return Object.getPrototypeOf(ele).name === 'Error';
 };
 
-var isNodeList = exports.isNodeList = function isNodeList(ele) {
-  return toStr.call(ele) === '[object NodeList]';
-};
-
 var isIterable = exports.isIterable = function isIterable(ele) {
   return (typeof ele === 'undefined' ? 'undefined' : _typeof(ele)) === 'object';
 };
@@ -156,3 +138,5 @@ var isIterable = exports.isIterable = function isIterable(ele) {
 var isEmail = exports.isEmail = function isEmail(ele) {
   return typeof ele === 'string' && ele.indexOf('@') > 0;
 };
+
+//# sourceMappingURL=index.js.map
