e613e0bc05ca1698fbbc10c73100978850679e84
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-16 07:58:24 +0200

    update magic/test, remove prettier config

--- test/index.js
+++ test/index.js
@@ -1,11 +1,3 @@
-const run = require("@magic/test")
-
-const types = require("./spec")
-const prettierConfig = require("./prettier")
-
-const tests = {
-  types,
-  prettierConfig
+module.exports = {
+  spec: require('./spec'),
 }
-
-run(tests)
