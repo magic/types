2799672467dd0751af890ba19f96ba73ac260e1a
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-30 19:35:44 +0200

    remove index files\

--- test/deep/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  equal: require('./equal'),
-  different: require('./different'),
-}
--- test/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  spec: require('./spec'),
-  primitives: require('./primitives'),
-  types: require('./types'),
-  colors: require('./colors'),
-  util: require('./util'),
-  deep: require('./deep'),
-}
