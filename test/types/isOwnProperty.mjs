b6d892fe78b77f58194620becf68baaf466ee6b9
jascha <j@jaeh.at>
2021-12-25 16:36:44 +0100

    format

--- test/types/isOwnProperty.mjs
+++ test/types/isOwnProperty.mjs
@@ -1,8 +1,19 @@
-import { isOwnProp, isOwnProperty } from "../../src/fns.mjs";
+import { isOwnProp, isOwnProperty } from '../../src/fns.mjs'
 
 export default [
-  { fn: isOwnProperty(undefined, 'try'), expect: false, info: 'isOwnProperty without argument returns false' },
-  { fn: isOwnProp({ test: undefined }, 'test'), info: 'isOwnProperty returns true for undefined but existing keys' },
+  {
+    fn: isOwnProperty(undefined, 'try'),
+    expect: false,
+    info: 'isOwnProperty without argument returns false',
+  },
+  {
+    fn: isOwnProp({ test: undefined }, 'test'),
+    info: 'isOwnProperty returns true for undefined but existing keys',
+  },
   { fn: isOwnProp({ test: true }, 'test'), info: 'isOwnProperty returns true for defined keys' },
-  { fn: isOwnProp({ test: true }, 'test2'), expect: false, info: 'isOwnProperty with non-existant key returns false' },
+  {
+    fn: isOwnProp({ test: true }, 'test2'),
+    expect: false,
+    info: 'isOwnProperty with non-existant key returns false',
+  },
 ]
