21bdf5d202d6c364215dbb45db5c01d904d1b463
jaeh <jascha@jaeh.at>
2022-05-07 17:33:59 +0200

    add is.falsy tests for empty objects and arrays

--- test/types/isFalsy.mjs
+++ test/types/isFalsy.mjs
@@ -5,10 +5,13 @@ export default [
   { fn: () => is.isFalsy(''), expect: true },
   { fn: () => is.falsy(''), expect: true },
   { fn: () => is.falsy(0), expect: true },
+  { fn: () => is.falsy({}), expect: true, },
+  { fn: () => is.falsy([]), expect: true, },
   { fn: () => is.falsy(false), expect: true },
   { fn: () => is.falsy('false'), expect: false },
   { fn: () => is.falsy('test'), expect: false },
   { fn: () => is.falsy(1), expect: false },
   { fn: () => is.falsy(['test']), expect: false },
   { fn: () => is.falsy({ t: 't' }), expect: false },
+  { fn: () => is.falsy({ slug: true, t: { tt: { ttt: 't' }}}), expect: false },
 ]
